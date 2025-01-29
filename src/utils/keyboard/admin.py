175e9cbe8729969948e9013e1bcb679a2694e2df
yarolys <lysov.yaroslav.r@mail.ru>
2025-01-29 22:33:37 +0800

    feat(keyboard): simplify admin panel layout
    Removed options for managing buttons, settings, and discounts from the admin panel keyboard.

--- src/utils/keyboard/admin.py
+++ src/utils/keyboard/admin.py
@@ -2,22 +2,11 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 admin_panel_kb = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Управление кнопками"),
-         KeyboardButton(text="Управление текстами")],
+        [KeyboardButton(text="Управление текстами")],
         [KeyboardButton(text="Список пользователей")],
         [KeyboardButton(text="Аналитика"),
-         KeyboardButton(text="Настройки")],
-        [KeyboardButton(text="Управление скидками")],
-
+        ]
     ],
     resize_keyboard=True
 )
 
-control_buttons_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='Добавить кнопку'),
-         KeyboardButton(text='Удалить кнопку')],
-        [KeyboardButton(text='Назад')]
-    ],
-    resize_keyboard=True
-)
