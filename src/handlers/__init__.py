83898cefbefcd0fe842cfa52d0e8d9de1b634d50
yarolys <lysov.yaroslav.r@mail.ru>
2025-01-29 16:06:45 +0800

    added control_buttons_router

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,3 +1,4 @@
 from .start import router as start_router
 from .panel_for_user.user_panel import router as user_panel_router
 from .panel_for_admin.admin_panel import router as admin_panel_router
+from .panel_for_admin.control_buttons import router as control_buttons_router
