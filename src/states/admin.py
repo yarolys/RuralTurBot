2b0d0dcd51194382e2a27490aa374af551262d51
yarolys <lysov.yaroslav.r@mail.ru>
2025-01-31 01:05:58 +0800

    feat(states): add states for managing tours and accommodation
    - Introduced `AddTourState`, `DeleteTourState`, and `AddAccomodationState` for handling tour and accommodation data management.
    - Added states for tour name and description, and accommodation name and description.

--- src/states/admin.py
+++ src/states/admin.py
@@ -3,3 +3,14 @@ from aiogram.fsm.state import StatesGroup, State
 
 class EditTextState(StatesGroup):
     waiting_for_new_text = State()
+
+class AddTourState(StatesGroup):
+    waiting_for_tour_name = State()
+    waiting_for_tour_description = State()
+
+class DeleteTourState(StatesGroup):
+    delete_tour = State()
+
+class AddAccomodationState(StatesGroup):
+    waiting_for_accomodation_name = State()
+    waiting_for_accomodation_description = State()
