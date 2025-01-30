9eaa46d2ed7e13f45807a2517d2a228bca9340a8
yarolys <lysov.yaroslav.r@mail.ru>
2025-01-31 01:07:13 +0800

    feat: update start panel and add new navigation options
    - Added "⛺️ Туры" button to the start panel for accessing tours.
    - Created accommodation panel (currently empty) for future accommodation options.
    - Added back-to-excursions and back-to-main buttons for navigation.

--- src/utils/keyboard/user.py
+++ src/utils/keyboard/user.py
@@ -5,12 +5,31 @@ from aiogram.types import (
 
 start_panel_kb = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text='🏡 Проживание', callback_data='accommodation')],
+        [InlineKeyboardButton(text='🏡 Проживание', callback_data='accommodation'),
+         InlineKeyboardButton(text='⛺️ Туры', callback_data='tour')],
         [InlineKeyboardButton(text='🍲 Местная кухня', callback_data='local_food'),
          InlineKeyboardButton(text='🗺 Экскурсии', callback_data='excursions')],
         [InlineKeyboardButton(text='🎉 Развлечения', callback_data='entertainment'),
          InlineKeyboardButton(text='🎁 Получить скидку', callback_data='get_discount')],
         [InlineKeyboardButton(text='💬 Контакты', callback_data='contacts'),
          InlineKeyboardButton(text='ℹ️ О нас', callback_data='about_us')]
+    ]
+)
+
+accomodation_panel_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='')]
+    ]
+)
+
+back_to_excursions = InlineKeyboardMarkup(
+        inline_keyboard=[
+                [InlineKeyboardButton(text="🔙 Вернуться", callback_data="excursions")]
+            ]
+)
+
+back_to_main = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="🔙 Вернуться", callback_data="back_to_main")]
     ]
 )
