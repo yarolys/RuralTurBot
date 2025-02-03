d0bed3adcecf93a50fa74303c2944969104371ab
yarolys <lysov.yaroslav.r@mail.ru>
2025-02-03 17:24:39 +0800

    feat(keyboard): deleted 1 button and swapped the buttons to make it look better

--- src/utils/keyboard/user.py
+++ src/utils/keyboard/user.py
@@ -7,10 +7,9 @@ start_panel_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text='🏡 Проживание', callback_data='accommodation'),
          InlineKeyboardButton(text='⛺️ Туры', callback_data='tour')],
-        [InlineKeyboardButton(text='🍲 Местная кухня', callback_data='local_food'),
-         InlineKeyboardButton(text='🗺 Экскурсии', callback_data='excursions')],
         [InlineKeyboardButton(text='🎉 Развлечения', callback_data='entertainment'),
-         InlineKeyboardButton(text='🎁 Получить скидку', callback_data='get_discount')],
+         InlineKeyboardButton(text='🍲 Местная кухня', callback_data='local_food')],
+        [InlineKeyboardButton(text='🎁 Получить скидку', callback_data='get_discount')],
         [InlineKeyboardButton(text='💬 Контакты', callback_data='contacts'),
          InlineKeyboardButton(text='ℹ️ О нас', callback_data='about_us')]
     ]
